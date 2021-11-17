10dfc9b99e696968708356a415ea5e146c3506d8
Ryan Taheri <sailantht@gmail.com>
2021-11-17 17:04:43 +0330

    getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import Link from "next/link";
 import Layout from "@/components/Layout";
+import {API_URL} from "@/config/index";
 
-export default function HomePage() {
-  return (
-    <Layout>
-      <h1>Home</h1>
-        <Link href='/about'>About</Link>
-    </Layout>
-  )
+export default function HomePage({events}) {
+    return (
+        <Layout>
+            <h1>Upcoming Events</h1>
+
+        </Layout>
+    )
+}
+
+export async function getStaticProps() {
+    const res = await fetch(`${API_URL}/api/events`)
+    const events = await res.json()
+
+    return {
+        props: {events},
+        revalidate: 1
+    }
 }
