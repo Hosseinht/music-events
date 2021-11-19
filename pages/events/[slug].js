49671e0bbbc9d96b80f59aa17607db55952bb550
Ryan Taheri <sailantht@gmail.com>
2021-11-19 15:52:28 +0330

    Delete Events

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -4,11 +4,26 @@ import {FaPencilAlt, FaTimes} from "react-icons/fa";
 import Layout from "@/components/Layout";
 import {API_URL} from "@/config/index";
 import styles from '@/styles/Event.module.css'
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {useRouter} from "next/router";
 
 
-const EventPage = ({evt}) => {
-    const deleteEvent = (e) => {
-        console.log('delete')
+const EventPage =  ({evt}) => {
+    const router = useRouter()
+    const deleteEvent = async (e) => {
+        if(confirm("Are you sure?")){
+            const res = await fetch(`${API_URL}/events/${evt.id}`, {
+                method: "Delete",
+            })
+            const data = await res.json()
+
+            if(!res.ok){
+                toast.error(data.message)
+            } else {
+                router.push('/events')
+            }
+        }
     }
 
     return (
@@ -24,6 +39,7 @@ const EventPage = ({evt}) => {
                     {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
                 </span>
                 <h1>{evt.name}</h1>
+                <ToastContainer/>
                 {evt.image && (
                     <div className={styles.image}>
                         <Image src={evt.image.formats.large.url} width={960} height={600}/>
