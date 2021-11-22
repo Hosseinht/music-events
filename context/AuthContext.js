10397bb8fb9504803afa757e81ab800ec1872a7c
Ryan Taheri <sailantht@gmail.com>
2021-11-22 16:00:49 +0330

    Logout & Destroy Cookie

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -47,7 +47,14 @@ export const AuthProvider = ({children}) => {
 
     // Logout user
     const logout = async () => {
-        console.log('logout')
+        // Call api route
+        const res = await fetch(`${NEXT_URL}/api/logout`, {
+            method: 'POST'
+        })
+        if (res.ok) {
+            setUser(null)
+            router.push('/')
+        }
     }
 
     // Check if user is logged in
